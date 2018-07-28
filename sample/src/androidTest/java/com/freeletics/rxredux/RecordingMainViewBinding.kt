b6d4307f3bc27f28a6928708442173ff19619ea7
sockeqwe <hannes.dorfmann@gmail.com>
2018-07-28 20:59:36 +0200

    Added screenshot tests

--- sample/src/androidTest/java/com/freeletics/rxredux/RecordingMainViewBinding.kt
+++ sample/src/androidTest/java/com/freeletics/rxredux/RecordingMainViewBinding.kt
@@ -1,18 +1,11 @@
 package com.freeletics.rxredux
 
-import android.app.Activity
-import android.content.ContextWrapper
-import android.graphics.Bitmap
-import android.os.Environment
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import com.facebook.testing.screenshot.Screenshot
 import com.freeletics.rxredux.businesslogic.pagination.PaginationStateMachine
 import io.reactivex.Observable
 import io.reactivex.subjects.ReplaySubject
-import timber.log.Timber
-import java.io.File
-import java.io.FileOutputStream
 import java.util.*
 
 
@@ -22,6 +15,7 @@ class RecordingMainViewBinding(rootView: ViewGroup) : MainViewBinding(rootView) 
     }
 
     val drawListener = ScreenshotTakingDrawListener()
+
     init {
         INSTANCE = this // I'm just to lazy to setup dagger properly :(
         rootView.viewTreeObserver.addOnDrawListener(drawListener)
@@ -42,11 +36,7 @@ class RecordingMainViewBinding(rootView: ViewGroup) : MainViewBinding(rootView) 
             if (queue.isNotEmpty()) {
                 Screenshot.snap(rootView).setName("MainView State ${subject.values.size + 1}")
                     .record()
-
                 val state = queue.poll()
-                //takeScreenshot()
-                //rootView.viewTreeObserver.removeOnDrawListener(this)
-                //takeScreenshot()
                 subject.onNext(state)
             }
         }
