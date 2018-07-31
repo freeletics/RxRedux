4a1327bc73ebaad8b9e21e793908ba90a8832f7f
sockeqwe <hannes.dorfmann@gmail.com>
2018-07-31 13:42:19 +0200

    Deployment

--- .circleci/deploy_snapshot.sh
+++
@@ -1,9 +0,0 @@
-cd library #Changing directory to your working directory
-File=library/gradle.properties
-if grep -q STRING_YOU_ARE_CHECKING_FOR "$File"; ##note the space after the string you are searching for
-then
-echo "Hooray!! Going to release the snapshot"
-./gradlew uploadArtifacts
-else
-echo "Skipping deploying snapshot because it is a RELEASE --> Hooray! --> Workflow to publish RELEASE will be triggered"
-fi
