0d83574a99ec2d74dcf6f43f0ee852aaee608184
sockeqwe <hannes.dorfmann@gmail.com>
2018-07-31 21:39:03 +0200

    The answer is y

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ else
   echo $PGP_KEY | gpg --passphrase-fd 0 --import Freeletics.asc
   rm Freeletics.asc
   gpg --list-keys
-  yes | gpg --delete-key D39DC0E3
+  y | gpg --delete-key D39DC0E3
   gpg --list-keys
   echo "Snapshot deployed!"
 fi
