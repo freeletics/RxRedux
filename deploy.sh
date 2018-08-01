ccdf7ef377f3508465a0957f6eeca90c3ec0755f
sockeqwe <hannes.dorfmann@gmail.com>
2018-08-01 02:36:12 +0200

    Don't deploy in Parallel to ensure all artifacts land in the same staging repo

--- deploy.sh
+++ deploy.sh
@@ -24,8 +24,9 @@ else
   openssl aes-256-cbc -K $encrypted_06b1e6b9a94a_key -iv $encrypted_06b1e6b9a94a_iv -in freeletics.gpg.enc -out freeletics.gpg -d
   echo "signing.password=$PGP_KEY" >> library/gradle.properties
   echo "signing.secretKeyRingFile=$PWD/freeletics.gpg" >> library/gradle.properties
+  echo "org.gradle.parallel=false" >> library/gradle.properties
   ./gradlew :library:uploadArchives
   rm freeletics.gpg
   git reset --hard
-  echo "Snapshot deployed!"
+  echo "Deployed!"
 fi
